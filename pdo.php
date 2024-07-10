93a82c6ce3fa63eca0ee41717301a84a1f59e210
Leopoldo Facci         (Dantallion) <111834303+Dantallion420@users.noreply.github.com>
2024-07-10 10:39:27 -0400

    pdo.php

--- pdo.php
+++ pdo.php
@@ -1,9 +1,9 @@
 <?php
 $host = 'localhost'; // Your host (e.g., localhost)
-$port = '8889'; // Your port number (if different from default)
+$port = ''; // Your port number (if different from default)
 $db = 'sunandmoon'; // Your database name
-$user = 'root'; // Your database username
-$password = 'root'; // Your database password
+$user = ''; // Your database username
+$password = ''; // Your database password
 
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $password);
